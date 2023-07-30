5053a5fc4de0e6234463b2fee9b4181505c4deb1
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2023-07-30 01:06:10 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <h1>Política de Privacidade</h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <p>
+        Bem-vindo(a) ao nosso aplicativo! Esta Política de Privacidade descreve
+        como coletamos, usamos e protegemos suas informações ao utilizar a
+        funcionalidade de gravação de áudio do aplicativo. Ao utilizar o recurso
+        de gravação de áudio, você concorda com os termos descritos nesta
+        política.
+      </p>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <h2>Coleta de Dados e Uso</h2>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <p>
+        Nosso aplicativo utiliza a permissão{" "}
+        <code>android.permission.RECORD_AUDIO</code> para acessar o microfone do
+        seu dispositivo. É importante ressaltar que{" "}
+        <strong>não armazenamos nem compartilhamos</strong> quaisquer
+        informações de áudio gravadas pelo aplicativo. Toda a gravação de áudio
+        é processada e permanece{" "}
+        <strong>exclusivamente no seu dispositivo</strong>, sem ser enviada a
+        nossos servidores ou a terceiros.
+      </p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+      <h2>Controle do Usuário</h2>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p>
+        Você tem total controle sobre quando e como o recurso de gravação de
+        áudio é utilizado. O aplicativo solicitará sua permissão para acessar o
+        microfone durante o tempo de execução. Você pode optar por conceder ou
+        negar essa permissão por meio das configurações do seu dispositivo.
+      </p>
+
+      <h2>Finalidade da Gravação de Áudio</h2>
+
+      <p>
+        A gravação de áudio é utilizada exclusivamente para{" "}
+        <strong>fins específicos</strong> dentro do aplicativo, como fornecer
+        recursos e funcionalidades que dependem do acesso ao microfone. Esses
+        recursos podem incluir, mas não estão limitados a: gravação de notas de
+        voz, gravação de áudio em um contexto específico dentro do aplicativo e
+        outras funcionalidades relacionadas.
+      </p>
+
+      <h2>Segurança e Privacidade</h2>
+
+      <p>
+        A segurança e a privacidade dos seus dados são fundamentais para nós. O
+        aplicativo utiliza medidas de segurança adequadas para proteger
+        quaisquer informações sensíveis que são processadas pelo recurso de
+        gravação de áudio.{" "}
+        <strong>Nenhuma informação de áudio é transmitida pela internet</strong>{" "}
+        ou armazenada em nossos servidores.
+      </p>
+
+      <h2>Atualizações na Política de Privacidade</h2>
+
+      <p>
+        Esta Política de Privacidade pode ser atualizada periodicamente para
+        refletir quaisquer mudanças em nossas práticas de privacidade. Quando
+        fizermos alterações significativas nesta política, notificaremos você
+        por meio de uma atualização no aplicativo ou de outra forma apropriada.
+        Certifique-se de revisar esta política regularmente para estar ciente de
+        como protegemos suas informações.
+      </p>
+
+      <h2>Contato</h2>
+
+      <p>
+        Se tiver alguma dúvida ou preocupação sobre nossa Política de
+        Privacidade ou sobre o uso do recurso de gravação de áudio do
+        aplicativo, entre em contato conosco através dos canais de suporte
+        fornecidos no aplicativo.
+      </p>
+
+      <p>Data de vigência: 01-01-2023</p>
+
+      <h1>Termos de Uso</h1>
+
+      <p>
+        1. <strong>Aceitação dos Termos</strong>
+      </p>
+
+      <p>
+        Ao utilizar o recurso de gravação de áudio neste aplicativo, você
+        concorda com estes Termos de Uso. Se você não concorda com qualquer
+        parte destes termos, não utilize o recurso de gravação de áudio.
+      </p>
+
+      <p>
+        2. <strong>Uso Responsável</strong>
+      </p>
+
+      <p>
+        Você concorda em utilizar o recurso de gravação de áudio apenas para
+        fins lícitos e de acordo com todas as leis e regulamentos aplicáveis.
+        Você não deve utilizar o recurso de gravação de áudio para fins ilegais
+        ou prejudiciais.
+      </p>
+
+      <p>
+        3. <strong>Propriedade Intelectual</strong>
+      </p>
+
+      <p>
+        O aplicativo e seu recurso de gravação de áudio são protegidos por
+        direitos autorais e outras leis de propriedade intelectual. Você
+        concorda em respeitar todos os direitos autorais e outras informações de
+        propriedade exibidas no aplicativo.
+      </p>
+
+      <p>
+        4. <strong>Isenção de Responsabilidade</strong>
+      </p>
+
+      <p>
+        O aplicativo não assume qualquer responsabilidade por danos ou prejuízos
+        decorrentes do uso do recurso de gravação de áudio ou de qualquer
+        problema técnico relacionado ao mesmo. O recurso de gravação de áudio é
+        fornecido "como está", e o aplicativo não faz nenhuma garantia quanto à
+        sua disponibilidade, precisão ou confiabilidade.
+      </p>
+
+      <p>
+        5. <strong>Alterações nos Termos de Uso</strong>
+      </p>
+
+      <p>
+        Estes Termos de Uso podem ser atualizados de tempos em tempos para
+        refletir mudanças no aplicativo ou em nossas práticas. Ao continuar a
+        utilizar o recurso de gravação de áudio após qualquer atualização destes
+        termos, você concorda em ficar vinculado às versões revisadas dos Termos
+        de Uso.
+      </p>
+
+      <p>
+        6. <strong>Contato</strong>
+      </p>
+
+      <p>
+        Se tiver alguma dúvida ou dúvida sobre estes Termos de Uso ou sobre o
+        uso do recurso de gravação de áudio do aplicativo, entre em contato
+        conosco por meio dos canais de suporte fornecidos no aplicativo.
+      </p>
+
+      <p>Data de vigência: 01-01-2023</p>
     </main>
-  )
+  );
 }
